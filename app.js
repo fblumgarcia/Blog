c3cf937e1a1d4593ee49e615063ff5f3c6e65c59
fblumgarcia <fblumgarcia0920@gmail.com>
2022-03-07 15:28:20 -0500

    Se agrega base de datos de MongoDB

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose=require("mongoose");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -17,8 +18,22 @@ app.use(express.static("public"));
 
 const posts=[];
 
+mongoose.connect("mongodb://localhost:27017/blogDB");//Conecta a base datos
+
+const postSchema=new mongoose.Schema({//Se crea el esquema de la base de datos
+    name:String,
+    content:String
+});
+
+const Post=mongoose.model("Post",postSchema);//Se crea el modelo constructor
+
 app.get("/",function(req,res){//Se crea las paginas
-  res.render("home",{startingContent:homeStartingContent,posts2:posts});//Siempre que se añade algo nuevo se mete acá
+  Post.find({},function(err,postsFound){
+    if(!err){
+      res.render("home",{startingContent:homeStartingContent,posts2:postsFound});//Siempre que se añade algo nuevo se mete acá
+    }
+  });
+  
 });
 app.get("/about",function(req,res){
   res.render("about",{aboutContent:aboutContent});
@@ -30,29 +45,35 @@ app.get("/compose",function(req,res){
   res.render("compose",{});
 });
 app.get("/post/:postName",function(req,res){
-  if(posts.find(x=>x.title==req.params.postName.toUpperCase())){//Busca el parametro postName
-    res.render("post",
-    {title2:req.params.postName.toUpperCase(),
-    post2:posts.find(x=>x.title==req.params.postName.toUpperCase()).content})//Crea la pagína con cierto post
-  }
+  postName=req.params.postName;
+  Post.findOne({_id:postName},function(err,postFound){
+    if(!err){//Busca el parametro postName
+      res.render("post",{post2:postFound})//Crea la pagína con cierto post
+    }
+  })
+  
 });
 
 app.post("/compose",function(req,res){
-  let post={//Crea un objeto con la info recolectada de compose
-    title:req.body.postTitle.toUpperCase(),
+  
+  const post=new Post({//Crea un objeto con la info recolectada de compose
+    name:req.body.postTitle.toUpperCase(),
     content:req.body.postBody
-  }
-  posts.push(post);
-  res.redirect("/");//Redirige a pagina principal
-})
-
-
-
-
-
+  });
+  post.save(function(err){
+    if(!err){
+      res.redirect("/");//Redirige a pagina principal
+    }
+  });
+});
 
 
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
+
+
+
+
+
